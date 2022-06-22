7a6139f174277f336eb0f858e875f2c5c987b159
iwdael <iwdael@outlook.com>
2022-06-22 10:25:17 +0800

    DbRoom

---
+++ app/src/main/java/com/iwdael/dbroom/example/entity/Music.kt
@@ -0,0 +1,33 @@
+package com.iwdael.dbroom.example.entity
+
+import androidx.databinding.Bindable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.iwdael.dbroom.annotations.UseDataBinding
+import com.iwdael.dbroom.annotations.UseNotifier
+import com.iwdael.dbroom.annotations.UseRoom
+import com.iwdael.dbroom.core.Notifier
+import kotlin.Long
+import kotlin.String
+
+@Entity
+@UseDataBinding
+@UseRoom
+@UseNotifier
+open class Music : Notifier {
+  @PrimaryKey
+  @Bindable
+  open var id: Long? = null
+
+  @Bindable
+  open var name: String? = null
+
+  @Bindable
+  open var artists: String? = null
+
+  @Bindable
+  open var duration: String? = null
+
+  @Bindable
+  open var lyrics: String? = null
+}
---
+++ core/src/main/java/com/iwdael/dbroom/core/SqlUnitPackingDouble.java
@@ -0,0 +1,72 @@
+package com.iwdael.dbroom.core;
+
+import java.util.Arrays;
+
+/**
+ * @author  : iwdael
+ * @mail    : iwdael@outlook.com
+ * @project : https://github.com/iwdael/dbroom
+ */
+public final class SqlUnitPackingDouble<CONVERT, SOURCE, TARGET, MAPPER> extends SqlUnit<CONVERT, SOURCE, Double, TARGET, MAPPER> {
+
+    public SqlUnitPackingDouble(SOURCE target, Column<Double> column, CallBack<SqlUnit<CONVERT, SOURCE, ?, TARGET, ?>> callBack, SqlCreator<CONVERT, SOURCE, TARGET> builder, SelectionCreator<SOURCE, TARGET> selectionCreator, SelectionCreator<SqlUnit<CONVERT, SOURCE, Double, TARGET, MAPPER>, MAPPER> operatorCreator) {
+        super(target, column, callBack, builder, selectionCreator, operatorCreator);
+    }
+
+    public MAPPER equal(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = EQUAL;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER unequal(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = UNEQUAL;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER greater(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = GREATER;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER greaterEqual(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = GREATER_EQUAL;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER less(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = LESS;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER lessEqual(Double value) {
+        this.value.add(value);
+        this.call.call(this);
+        this.assign = LESS_EQUAL;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER between(Double value1, Double value2) {
+        this.value.add(value1);
+        this.value.add(value2);
+        this.call.call(this);
+        this.assign = BETWEEN;
+        return operatorCreator.create(this);
+    }
+
+    public MAPPER in(Double... value) {
+        this.value.addAll(Arrays.asList(value));
+        this.call.call(this);
+        this.assign = IN;
+        return operatorCreator.create(this);
+    }
+}
