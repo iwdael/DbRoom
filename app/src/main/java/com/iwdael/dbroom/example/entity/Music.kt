cb823e96602484bd2e194532c4695f1f6bdb4578
iwdael <iwdael@outlook.com>
2024-11-27 10:46:29 +0800

    ksp version

--- app/src/main/java/com/iwdael/dbroom/example/entity/Music.kt
+++ app/src/main/java/com/iwdael/dbroom/example/entity/Music.kt
@@ -1,33 +1,14 @@
 package com.iwdael.dbroom.example.entity
 
-import androidx.databinding.Bindable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.iwdael.dbroom.annotations.UseDataBinding
-import com.iwdael.dbroom.annotations.UseNotifier
-import com.iwdael.dbroom.annotations.UseRoom
-import com.iwdael.dbroom.core.Notifier
-import kotlin.Long
-import kotlin.String
 
 @Entity
-@UseDataBinding
-@UseRoom
-@UseNotifier
-open class Music : Notifier {
-  @PrimaryKey
-  @Bindable
-  open var id: Long? = null
-
-  @Bindable
-  open var name: String? = null
-
-  @Bindable
-  open var artists: String? = null
-
-  @Bindable
-  open var duration: String? = null
-
-  @Bindable
-  open var lyrics: String? = null
+class Music {
+    @PrimaryKey
+    var id: Long? = null
+    var name: String? = null
+    var artists: String? = null
+    var duration: String? = null
+    var lyrics: String? = null
 }
